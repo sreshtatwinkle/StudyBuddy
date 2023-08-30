24627bb88139179718501bda574377b6d2e16b4f
sreshtaP <sreshta.twinkle@gmail.com>
2023-08-30 02:13:21 +0100

    adding profile page related code

--- Studybuddy/settings.py
+++ Studybuddy/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Studybuddy_main_app',
     'accounts',
+    'chat_app',
 ]
 
 MIDDLEWARE = [
@@ -130,6 +131,8 @@ STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('register',views.register_form,name='register'),
     path('login',views.login_form,name='login'),
-    path('logout',views.logout,name='logout')
+    path('logout',views.logout,name='logout'),
+    
 ]
